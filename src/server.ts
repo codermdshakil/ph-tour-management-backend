5faa67c88eab37d35e01a7abe8335a5de8f4d9b6
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-12-23 21:17:26 +0600

    Error Handled: Handle Unhandled rejection error

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,10 @@ import app from "./app";
 
 let server: Server;
 
-
 const startServer = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://fnfteam:fnfteam@fnfteam.ip63dn0.mongodb.net/tour-management-backend"
+      "mongodb+srv://fnfteam:fnfteam@fnfteam.ip63dn0.mongodb.net/tour-db"
     );
     console.log("Database Connected successfully!!");
 
@@ -20,16 +19,26 @@ const startServer = async () => {
   }
 };
 
-
 startServer();
+
+// handle unhandled rejection error
+process.on("unhandledRejection", () => {
+
+  console.log("Unhandle Rejection detected!! Server shutting down...");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  
+});
 
 /**
  * 3 types of error can be occured!
- * 
+ *
  * - Unhandled rejection error
- * - Uncaught rejection error 
+ * - Uncaught rejection error
  * - Signal Termination / Sigterm
- * 
- * 
-*/
-
+ *
+ */
