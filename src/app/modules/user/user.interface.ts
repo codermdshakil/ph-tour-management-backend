5c55cc9d9c12a7f52745b02bf60c541597f2647f
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-01-02 21:33:54 +0600

    Create: User Interface

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,16 +1,40 @@
-export interface IUser {
-  name: string;
-  email: string;
-  password?: string;
-  phone?: string;
-  picture?: string;
-  address?: string;
-  isDeleted?: boolean;
-  isVerified?: boolean;
-  isActive?:string;
-  
-  auths?: "Google" | "Facebook";
-  role: "Admin" | "User";
-  booking:string,
+import { Types } from "mongoose";
 
+export enum Role{
+  ADMIN="ADMIN",
+  SUPER_ADMIN="SUPER_ADMIN",
+  USER="USER",
+  GUIDE="GUIDE"
+};
+
+// auth providers
+// - email, password
+// - google authentication
+
+export interface IAuthProvider{
+  provider:string; // "Google", "Credential"
+  providerId:string;
+};
+
+export enum IsActive{
+  ACTIVE="ACTIVE",
+  INACTIVE="INACTIVE",
+  BLOCKED="BLOCKED"
+}
+
+
+export interface IUser{
+  name:string;
+  email:string;
+  password?:string;
+  phone?:string;
+  picture?:string;
+  address?:string;
+  isDeleted?:string;
+  isActive?:IsActive;
+  isVerified?:string;
+  role?:Role;
+  auths?:IAuthProvider[];
+  bookings?: Types.ObjectId[];
+  guides:Types.ObjectId[];
 }
