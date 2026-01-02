0dd76313d5eb430961afcb50f1b6cf36b3638aa8
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-01-02 22:27:17 +0600

    Create: User Controllers

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { User } from "./user.mode";
+
+
+
+// create user
+export const createUser = async (req:Request, res:Response) => {
+  try {
+
+    const {name, email} = req.body;
+    const user = await User.create({name, email});
+    console.log(user);
+
+    res.status(StatusCodes.CREATED).json({
+      message:"User created successfully!",
+      user:user
+    });
+
+    
+  } catch (err:any) {
+    console.log(err);
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message:`Something want wrong!! ${err.message}`,
+      err:err
+    })
+  }
+}
+
+
+export const userControllers = {
+  createUser
+}
+
