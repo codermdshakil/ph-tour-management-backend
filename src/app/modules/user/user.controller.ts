3303f6fb6ece1c979acf9eeaa492d36c89c3847a
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-01-03 21:04:37 +0600

    Create: UserService layer and connected

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,34 +2,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { User } from "./user.mode";
-
-
+import { UserServices } from "./user.service";
 
 // create user
-export const createUser = async (req:Request, res:Response) => {
+export const createUser = async (req: Request, res: Response) => {
   try {
-
-    const {name, email} = req.body;
-    const user = await User.create({name, email});
+    const user = await UserServices.createUser(req.body);
 
     res.status(StatusCodes.CREATED).json({
-      message:"User created successfully!",
-      user:user
+      message: "User created successfully!",
+      user: user,
     });
 
-    
-  } catch (err:any) {
+  } catch (err: any) {
     console.log(err);
     res.status(StatusCodes.BAD_REQUEST).json({
-      message:`Something want wrong!! ${err.message}`,
-      err:err
-    })
+      message: `Something want wrong!! ${err.message}`,
+      err: err,
+    });
   }
-}
-
+};
 
 export const UserControllers = {
-  createUser
-}
+  createUser,
+};
 
+// route matching - controller - service - model - DB
+ 
