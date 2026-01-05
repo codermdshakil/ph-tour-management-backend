7cee858844a328909a39678fdc344063bf8d5b1a
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-01-06 00:25:26 +0600

    validateRequest - using this validator function createUserZodSchema and updateUserZodSchema validate + senitaize using zod

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
+import { validateRequest } from "../../utils/validateRequest";
 import { UserControllers } from "./user.controller";
-
+import { createUserZodSchema } from "./user.validate";
 
 const router = Router();
 
-router.post("/register",UserControllers.createUser);
-router.get("/all-users", UserControllers.getAllUsers)
+
+router.post("/register", validateRequest(createUserZodSchema),UserControllers.createUser);
+
+router.get("/all-users", UserControllers.getAllUsers);
 
 export const UserRoutes = router;
-
-
-
