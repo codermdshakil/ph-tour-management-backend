51692fb5652411f209e23c1ff451accf6743633e
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-01-06 22:25:21 +0600

    Add authorization middleware to protect all-users route and handle token verification

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,12 +1,54 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errorHanlers/AppError";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { UserControllers } from "./user.controller";
+import { Role } from "./user.interface";
 import { createUserZodSchema } from "./user.validate";
 
 const router = Router();
 
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserControllers.createUser
+);
+router.get("/all-users",
+  (req: Request, res: Response, next: NextFunction) => {
 
-router.post("/register", validateRequest(createUserZodSchema),UserControllers.createUser);
-router.get("/all-users", UserControllers.getAllUsers);
+    try {
+
+      // 1. get access token
+      const accessToken = req.headers.authorization;
+
+      // handle error
+      if (!accessToken) {
+        throw new AppError(StatusCodes.BAD_REQUEST, "No token Recived!");
+      }
+
+      // 2. get verified token
+      const verifiedToken = jwt.verify(accessToken, "secret");
+
+      console.log(verifiedToken, "verifiedToken");
+
+      // handle error
+      if (!verifiedToken) {
+        throw new AppError(StatusCodes.BAD_REQUEST, "You are not Authorized!");
+      }
+
+      if((verifiedToken as JwtPayload).role !== Role.ADMIN || Role.SUPER_ADMIN){
+        throw new AppError(StatusCodes.BAD_REQUEST, "You are not permitted to view this Route!")
+      }
+  
+      next();
+
+    } catch (error) {
+      next(error)
+    }
+
+  },
+  UserControllers.getAllUsers
+);
 
 export const UserRoutes = router;
