5cf2673930345b14baca065fc2ca0e91b02bc81c
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-01-06 14:12:14 +0600

    HashedPassword: in register route error handle and hashedpassword

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import bcryptjs from "bcryptjs";
 import { StatusCodes } from "http-status-codes";
 import AppError from "../../errorHanlers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
@@ -5,24 +6,32 @@ import { User } from "./user.model";
 
 // create user
 const createUser = async (payload: Partial<IUser>) => {
-  const { email, ...rest } = payload;
 
+
+  const { email, password, ...rest } = payload;
+
+  // check user exist or not
   const isUserExist = await User.findOne({ email });
 
   if (isUserExist) {
     throw new AppError(StatusCodes.BAD_REQUEST, "User Already Exist!");
   }
 
+  // auth provider 
   const authProvider: IAuthProvider = {
     provider: "google",
     providerId: email as string,
   };
 
+  
+  const hashedPassword = await bcryptjs.hash(password as string, 10);
+
   const user = await User.create({
-     email,
-     auths:[authProvider],
-    ...rest
- });
+    email,
+    password: hashedPassword,
+    auths: [authProvider],
+    ...rest,
+  });
 
   return user;
 };
