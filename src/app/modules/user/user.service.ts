5db9786de202f5aa73dc9b98211a830d381d25c9
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-01-06 13:15:17 +0600

    Refactor IAuthProvider interface to enforce provider type and update createUser function for improved user creation logic

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,27 +1,43 @@
-import { IUser } from "./user.interface";
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../errorHanlers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 
 // create user
 const createUser = async (payload: Partial<IUser>) => {
-  const { name, email } = payload;
+  const { email, ...rest } = payload;
 
-  const user = await User.create({ name, email });
+  const isUserExist = await User.findOne({ email });
+
+  if (isUserExist) {
+    throw new AppError(StatusCodes.BAD_REQUEST, "User Already Exist!");
+  }
+
+  const authProvider: IAuthProvider = {
+    provider: "google",
+    providerId: email as string,
+  };
+
+  const user = await User.create({
+     email,
+     auths:[authProvider],
+    ...rest
+ });
+
   return user;
 };
 
 // get allUsers
 const getAllUsers = async () => {
-
   const users = await User.find({});
   const totalUsers = await User.countDocuments();
 
   return {
-    data:users,
-    meta:{
-      total:totalUsers
-    }
+    data: users,
+    meta: {
+      total: totalUsers,
+    },
   };
-
 };
 
 export const UserServices = {
