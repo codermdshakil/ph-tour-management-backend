305c0b620c042dbe7d32da8b0f27000afc0ddf5f
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-01-17 13:35:23 +0600

    Implement refresh token functionality and create user tokens utility for new AccessToken

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import { sendResponse } from "../../utils/sendResponse";
 import { AuthServices } from "./auth.service";
 
 
+// create user and get access and refresh token
 const credentialsLogin =  catchAsync( async (req: Request, res: Response, next: NextFunction) => {
 
   const loginInfo = await AuthServices.credentialsLogin(req.body)
@@ -20,6 +21,25 @@ const credentialsLogin =  catchAsync( async (req: Request, res: Response, next: 
 
 );
 
+// using refresh-token get new accessToken
+const getNewAccessToken =  catchAsync( async (req: Request, res: Response, next: NextFunction) => {
+
+  // from cookie get refreshToken
+  const refreshToken = req.headers.authorization as string;
+
+  const tokenInfo = await AuthServices.getNewAccessToken(refreshToken);
+
+    sendResponse(res, {
+      success:true,
+      statusCode:StatusCodes.OK,
+      message:"Succcessfully get AccessToken!!",
+      data:tokenInfo
+    })
+  }
+
+);
+
 export const AuthControllers = {
-  credentialsLogin
+  credentialsLogin,
+  getNewAccessToken
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import bcryptjs from "bcryptjs";
 import { StatusCodes } from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/env";
 import AppError from "../../errorHanlers/AppError";
-import { generateToken } from "../../utils/jwt";
-import { IUser } from "../user/user.interface";
+import { generateToken, verifyToken } from "../../utils/jwt";
+import { createUserTokens } from "../../utils/userTokens";
+import { IsActive, IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
@@ -28,27 +30,67 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
 
   // const { password: _password, ...rest } = isUserExist.toObject();
 
-  const jwtPayload = {
-    userId: isUserExist._id,
-    email: isUserExist.email,
-    role: isUserExist.role,
-  };
+  // const jwtPayload = {
+  //   userId: isUserExist._id,
+  //   email: isUserExist.email,
+  //   role: isUserExist.role,
+  // };
 
+  // const accessToken = generateToken(jwtPayload,envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
+  // const refreshToken = generateToken(jwtPayload,envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES);
 
-  const accessToken = generateToken(jwtPayload,envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
-  const refreshToken = generateToken(jwtPayload,envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES);
+  const userTokens = createUserTokens(isUserExist);
 
   // delete password from user   
   const { password: _password, ...rest } = isUserExist.toObject();
   // delete isUserExist.password;
 
   return {
-    accessToken,
-    refreshToken,
+    accessToken:userTokens.accessToken,
+    refreshToken:userTokens.refreshToken,
     user:rest
   };
 };
 
+// using refreshToken get new AccessToken
+const getNewAccessToken = async (refreshToken:string) => {
+
+  // 1. verify refreshtoken
+  const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload;
+  
+  // 2. check isUserExit?
+
+  const isUserExist = await User.findOne({email:verifiedRefreshToken.email});
+
+  if(!isUserExist){
+    throw new AppError(StatusCodes.BAD_REQUEST, "User not Found!!")
+  }
+
+  if(isUserExist.isActive ===  IsActive.BLOCKED || isUserExist.isActive ===  IsActive.INACTIVE){
+    throw new AppError(StatusCodes.BAD_REQUEST, `User is ${isUserExist.isActive}!!`)
+  }
+
+  if(isUserExist.isDeleted){
+    throw new AppError(StatusCodes.BAD_REQUEST, "User is Deleted!!")
+  }
+
+
+  // now generate new accessToken
+
+  const jwtPayload = {
+    userId:isUserExist._id,
+    email:isUserExist.email,
+    role:isUserExist.role
+  };
+
+  const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET,envVars.JWT_ACCESS_EXPIRES);
+
+  return {
+    accessToken:accessToken
+  }
+}
+
 export const AuthServices = {
   credentialsLogin,
+  getNewAccessToken
 };
