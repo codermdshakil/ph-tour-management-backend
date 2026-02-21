fd5d897d620c37122fe5cf92aec20e526df5c015
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-02-21 21:22:53 +0600

    Implement Transaction and Rollback :  transaction handling in createBooking: add session management, rollback on error, and payment creation

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { StatusCodes } from "http-status-codes";
 import AppError from "../../errorHanlers/AppError";
 import { PAYMENT_STATUS } from "../payment/payment.interface";
@@ -16,44 +17,78 @@ const getTransactionId = () => {
 const createBooking = async (payload: Partial<IBooking>, userId: string) => {
   const transactionId = getTransactionId();
 
-  const user = await User.findById(userId);
+  // Implment transaction and Rollback for Booking, Payment;
+  // 1. create session
+  // 2. try-catch block try block a sob operations
+  // 3. [Create / Update] er somoy - operations Booking.create([{bookingPayload}], {session}) er second parameter hisabe session add koro
+  // 4. session commit kora lagbe
+  //    - await session.commitTransaction();
+  //    - session.endSession();
 
-  // if(!user?.phone || !user?.address){
-  //   throw new AppError(StatusCodes.BAD_REQUEST, "Pleaes, Update your Profile to book a Tour!");
-  // };
+  const session = await Booking.startSession();
+  session.startTransaction();
 
-  const tour = await Tour.findById(payload.tour).select("costFrom");
+  try {
+    const user = await User.findById(userId);
 
-  if (!tour?.costFrom) {
-    throw new AppError(StatusCodes.BAD_REQUEST, "No not tour costFrom");
+    if (!user?.phone || !user?.address) {
+      throw new AppError(
+        StatusCodes.BAD_REQUEST,
+        "Pleaes, Update your Profile to book a Tour!",
+      );
+    }
+
+    const tour = await Tour.findById(payload.tour).select("costFrom");
+
+    if (!tour?.costFrom) {
+      throw new AppError(StatusCodes.BAD_REQUEST, "No not tour costFrom");
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const amount = Number(tour.costFrom) * Number(payload.guestCount!);
+
+    const booking = await Booking.create(
+      [
+        {
+          user: userId,
+          status: BOOKING_STATUS.PENDING,
+          ...payload,
+        },
+      ],
+      { session },
+    );
+
+    const payment = await Payment.create(
+      [
+        {
+          booking: booking[0]._id,
+          status: PAYMENT_STATUS.UNPAID,
+          transactionId: transactionId,
+          amount: amount,
+        },
+      ],
+      { session },
+    );
+
+    const updateBooking = await Booking.findByIdAndUpdate(
+      booking[0]._id,
+      { payment: payment[0]._id },
+      { new: true, runValidators: true, session },
+    )
+      .populate("user", "name email password role")
+      .populate("tour", "title costFrom")
+      .populate("payment");
+
+    await session.commitTransaction(); // transaction
+    session.endSession();
+
+    return updateBooking;
+  } catch (error:any) {
+    // ai khane jodi whole process a kono error hoy sob kisu bad diye dibe
+    await session.abortTransaction(); // rollback
+    session.endSession();
+    throw error;
   }
-
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const amount = Number(tour.costFrom) * Number(payload.guestCount!);
-
-  const booking = await Booking.create({
-    user: userId,
-    status: BOOKING_STATUS.PENDING,
-    ...payload,
-  });
-
-  const payment = await Payment.create({
-    booking: booking._id,
-    status: PAYMENT_STATUS.UNPAID,
-    transactionId: transactionId,
-    amount: amount,
-  });
-
-  const updateBooking = await Booking.findByIdAndUpdate(
-    booking._id,
-    { payment: payment._id },
-    { new: true, runValidators: true },
-  )
-    .populate("user", "name email password role")
-    .populate("tour", "title costFrom")
-    .populate("payment");
-
-  return updateBooking;
 };
 
 const updateBookingStatus = () => {};
