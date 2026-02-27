15bfb9690f2070ced4ea956a14ba656978ec53d8
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-02-28 02:14:50 +0600

    Implement payment initialization: enhance initPayment method to include user details and SSLCommerz integration, and update payment controller and routes accordingly

--- src/app/modules/payment/payment.routes.ts
+++ src/app/modules/payment/payment.routes.ts
@@ -4,6 +4,7 @@ import { paymentControllers } from "./payment.controller";
 
 const router = Router();
 
+router.post("/init-payment/:bookingId", paymentControllers.initPayment)
 router.post("/success", paymentControllers.successPayment);
 router.post("/fail", paymentControllers.failPayment);
 router.post("/cancel", paymentControllers.cancelPayment);
--- src/app/modules/payment/payment.services.ts
+++ src/app/modules/payment/payment.services.ts
@@ -1,8 +1,52 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../errorHanlers/AppError";
 import { BOOKING_STATUS } from "../booking/booking.interface";
 import { Booking } from "../booking/booking.model";
+import { ISSLCommerz } from "../sslCommerz/sslCommerz.interface";
+import { SSLService } from "../sslCommerz/sslCommerz.service";
+import { User } from "../user/user.model";
 import { PAYMENT_STATUS } from "./payment.interface";
 import { Payment } from "./payment.model";
+
+const initPayment = async (bookingId: string) => {
+  const resultPayment = await Payment.findOne({ booking: bookingId });
+
+  if (!resultPayment) {
+    throw new AppError(
+      StatusCodes.NOT_FOUND,
+      "Payment not found! You have not take this tour!",
+    );
+  }
+
+  const booking = await Booking.findById(bookingId);
+  const user = await User.findById(booking?.user);
+  const payment = await Payment.findById(booking?.payment);
+
+  const userAddress = (user as any).address;
+  const userEmail = (user as any).email;
+  const userPhoneNumber = (user as any).phone;
+  const userName = (user as any).name;
+  const userAmount = payment?.amount as number;
+  const userTransactionId = payment?.transactionId as string;
+
+  const sslPayload: ISSLCommerz = {
+    address: userAddress,
+    email: userEmail,
+    phoneNumber: userPhoneNumber,
+    name: userName,
+    amount: userAmount,
+    transactionId: userTransactionId,
+  };
+
+  const sslPayment = await SSLService.sslPaymentInit(sslPayload);
+
+  return {
+    paymentUrl: sslPayment.GatewayPageURL,
+  };
+
+  
+};
 
 const successPayment = async (query: Record<string, string>) => {
   // update booking status to Comfirm
@@ -87,14 +131,13 @@ const cancelPayment = async (query: Record<string, string>) => {
     await Booking.findOneAndUpdate(
       updatedPayment?.booking,
       { status: BOOKING_STATUS.CANCEL },
-      {   runValidators: true, session },
+      { runValidators: true, session },
     );
 
     await session.commitTransaction(); //transaction
     session.endSession();
 
     return { success: false, message: "Payment Cancel!" };
-
   } catch (error: any) {
     // ai khane jodi whole process a kono error hoy sob kisu bad diye dibe
     await session.abortTransaction(); // rollback
@@ -104,6 +147,7 @@ const cancelPayment = async (query: Record<string, string>) => {
 };
 
 export const paymentService = {
+  initPayment,
   successPayment,
   failPayment,
   cancelPayment,
